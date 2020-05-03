2180b40bad303de42a497c921023e05916c6af5e
cs8080 <cs8.cs8080.cscs8@gmail.com>
2020-05-03 17:31:52 +0900

    fix List13.3

--- src/main/kotlin/com/example/todolist/TaskController.kt
+++ src/main/kotlin/com/example/todolist/TaskController.kt
@@ -1,11 +1,12 @@
 package com.example.todolist
 
+import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("tasks")
 class TaskController {
     @GetMapping("")
