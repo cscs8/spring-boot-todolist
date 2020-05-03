3f75a10369aeabfaf8c31bf22071dc257544295a
cs8080 <cs8.cs8080.cscs8@gmail.com>
2020-05-03 17:57:02 +0900

    fix List 13.5 - 13.6

---
+++ src/main/kotlin/com/example/todolist/InMemoryTaskRepository.kt
@@ -0,0 +1,32 @@
+package com.example.todolist
+
+import com.fasterxml.jackson.databind.ObjectMapper
+
+class InMemoryTaskRepository : TaskRepository {
+    private val tasks: MutableList<Task> = mutableListOf()
+
+    private val maxId: Long
+        get() = tasks.map(Task::id).max() ?: 0
+
+    override fun create(content: String): Task {
+        val id = maxId + 1
+        val task = Task(id, content, false)
+        tasks += task
+        return task
+    }
+
+    override fun update(task: Task) {
+        tasks.replaceAll {
+            if (it.id == task.id) task
+            else it
+        }
+    }
+
+    // 元のリストを破壊されないように、コピーして新しいリストを返す
+    override fun findAll(): List<Task> = tasks.toList()
+
+
+    override fun findById(id: Long): Task? =
+            tasks.find { it.id == id }
+
+}
