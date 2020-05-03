d504baeba00ec34dd0985c3fb139338a21e429b6
cs8080 <cs8.cs8080.cscs8@gmail.com>
2020-05-03 17:30:03 +0900

    List13.1 - 13.3

---
+++ src/main/kotlin/com/example/todolist/TaskController.kt
@@ -0,0 +1,21 @@
+package com.example.todolist
+
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("tasks")
+class TaskController {
+    @GetMapping("")
+    fun index(model: Model): String {
+        val tasks = listOf(
+                Task(1, "障子をはりかえる", false),
+                Task(2, "定期健診にいく", true)
+        )
+        model.addAttribute("tasks", tasks)
+        return "tasks/index"
+    }
+
+}
