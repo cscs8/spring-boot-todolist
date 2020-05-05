50e19f495a0e56340b37a58d501378772ffcc48b
cs8080 <cs8.cs8080.cscs8@gmail.com>
2020-05-05 21:42:50 +0900

    List 15.4

--- src/test/kotlin/com/example/todolist/JdbcTaskRepositoryTest.kt
+++ src/test/kotlin/com/example/todolist/JdbcTaskRepositoryTest.kt
@@ -4,8 +4,10 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.jdbc.Sql
 
 @SpringBootTest
+@Sql(statements = ["DELETE FROM task"])
 class JdbcTaskRepositoryTest {
 
     @Autowired
@@ -15,5 +17,12 @@ class JdbcTaskRepositoryTest {
     fun 何も作成しなければfindAllは空のリストを作成すること() {
         val got = sut.findAll()
         assertThat(got).isEmpty()
+    }
+
+    @Test
+    fun createで作成したタスクをfindByIdで取得できること() {
+        val task = sut.create("TEST")
+        val got = sut.findById(task.id)
+        assertThat(got).isEqualTo(task)
     }
 }
