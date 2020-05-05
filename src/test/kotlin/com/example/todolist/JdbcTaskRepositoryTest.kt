039db818cc4b657a531759261cb13f3bc994b947
cs8080 <cs8.cs8080.cscs8@gmail.com>
2020-05-05 19:22:02 +0900

    List 15.3

--- src/test/kotlin/com/example/todolist/JdbcTaskRepositoryTest.kt
+++ src/test/kotlin/com/example/todolist/JdbcTaskRepositoryTest.kt
@@ -1,5 +1,7 @@
 package com.example.todolist
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,4 +10,10 @@ class JdbcTaskRepositoryTest {
 
     @Autowired
     private lateinit var sut: JdbcTaskRepository
+
+    @Test
+    fun 何も作成しなければfindAllは空のリストを作成すること() {
+        val got = sut.findAll()
+        assertThat(got).isEmpty()
+    }
 }
