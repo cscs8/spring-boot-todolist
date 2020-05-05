52ea07db5061c219751a124288eaebcd91b2cc33
cs8080 <cs8.cs8080.cscs8@gmail.com>
2020-05-05 21:49:34 +0900

    List 15.5

---
+++ src/test/kotlin/com/example/todolist/TaskControllerTest.kt
@@ -0,0 +1,19 @@
+package com.example.todolist
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+
+@WebMvcTest
+class TaskControllerTest {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private lateinit var taskRepository: TaskRepository
+
+    @MockBean
+    private lateinit var commandLineRunner: CommandLineRunner
+}
